16defffe3f5b35e5bfb945d6fc0f2647d0d47478
sheigel <eigel.si@gmail.com>
2016-03-19 21:21:14 +0200

    very simple test of redux-form

--- index.jsx
+++ index.jsx
@@ -1,13 +1,136 @@
-require("./node_modules/bootstrap/dist/css/bootstrap.min.css")
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component, PropTypes} from 'react';
+import {render} from 'react-dom';
+import {reduxForm, reducer as formReducer} from 'redux-form';
+import { createStore } from 'redux'
+import {connect} from 'react-redux'
+import { Provider } from 'react-redux'
+import { combineReducers } from 'redux'
 
-export class App extends React.Component {
-	render() {
-		return (
-			<div>Simple React + Babel + Bootstrap + Webpack</div>
-		);
-	}
+export const fields = ['username', 'email', 'age'];
+
+const validate = values => {
+    const errors = {};
+    if (!values.username) {
+        errors.username = 'Required';
+    } else if (values.username.length > 15) {
+        errors.username = 'Must be 15 characters or less';
+    }
+    if (!values.email) {
+        errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address';
+    }
+    if (!values.age) {
+        errors.age = 'Required';
+    } else if (isNaN(Number(values.age))) {
+        errors.age = 'Must be a number';
+    } else if (Number(values.age) < 18) {
+        errors.age = 'Sorry, you must be at least 18 years old';
+    }
+    return errors;
+};
+
+class SynchronousValidationForm extends Component {
+
+    render() {
+
+        const {fields: {username, email, age}, resetForm, handleSubmit, submitting} = this.props;
+        return (<form onSubmit={handleSubmit}>
+            <div>
+                <label>Username</label>
+                <div>
+                    <input type="text" placeholder="Username" {...username}/>
+                </div>
+                {username.error && <div>{username.error}</div>}
+            </div>
+            <div>
+                <label>Email</label>
+                <div>
+                    <input type="text" placeholder="Email" {...email}/>
+                </div>
+                {email.touched && email.error && <div>{email.error}</div>}
+            </div>
+            <div>
+                <label>Age</label>
+                <div>
+                    <input type="text" placeholder="Age" {...age}/>
+                </div>
+                {age.touched && age.error && <div>{age.error}</div>}
+            </div>
+            <div>
+                <button type="submit" disabled={submitting}>
+                    {submitting ? <i/> : <i/>} Submit
+                </button>
+                <button type="button" disabled={submitting} onClick={resetForm}>
+                    Clear Values
+                </button>
+            </div>
+        </form>
+        );
+    }
 }
 
-ReactDOM.render(<App/>, document.querySelector("#myApp"));
+function counter(state = 0, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1
+        case 'DECREMENT':
+            return state - 1
+        default:
+            return state
+    }
+}
+
+const store = createStore(combineReducers({
+    counter,
+    form: formReducer,
+    submitForm: (state = {},action) => {
+        console.log('called reducer',state,action);
+        return state;
+    }
+}))
+
+const Formul = reduxForm({
+    form: 'form',
+    fields,
+    validate,
+    onSubmit: ()=>{
+        
+    }
+}
+// , null, ()=>{
+//     return {
+//         onSubmit: (fields,dispatch) => {
+//             console.log('called mapped props',fields)
+//             return dispatch({type:'fuck'});
+//         }, onSubmit1: () => {
+//             console.log('called mapped props')
+//             return {action:'fuck'};
+//         },
+//         fu:'ck',
+//         handleSubmit:()=>{
+//             console.log("called handleSubmit")
+//         }
+// }}
+)(SynchronousValidationForm);
+
+const Wrapper = connect()(class WrapperPage extends React.Component {
+    render() {
+        return <div>
+            {this.props.children}
+        </div>
+    }
+})
+
+const el = document.querySelector("#myApp");
+console.log(el);
+
+
+
+render((
+    <Provider store={store}>
+        <Wrapper>
+            <Formul handleSubmit={console.log('called Formul handler') }/>
+        </Wrapper>
+    </Provider>
+), el);
--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,73 @@
 var webpack = require('webpack');
 var path = require('path');
+var OpenBrowserWebpackPlugin = require('open-browser-webpack-plugin');
 
 module.exports = {
-	entry: [
-		'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
-		'webpack/hot/only-dev-server',
-		'./index.jsx' // Your appʼs entry point
-	],
-	devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js'
-	},
-	resolve: {
-		extensions: ['', '.js', '.jsx']
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.jsx?$/,
-				exclude: /(node_modules|bower_components)/,
-				loaders: ['react-hot', 'babel'],
-			},
+    entry: [
+        'webpack-dev-server/client?http://0.0.0.0:8081', // WebpackDevServer host and port
+        'webpack/hot/only-dev-server',
+        './index.jsx' // Your appʼs entry point
+    ],
+    devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loaders: [ 'babel'],
+            },
 
-			{
-				test: /\.css$/,
-				loader: 'style-loader!css-loader'
-			},
-			{
-				test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-				loader: "file"
-			},
-			{
-				test: /\.(woff|woff2)$/,
-				loader: "url?prefix=font/&limit=5000"
-			},
-			{
-				test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-				loader: "url?limit=10000&mimetype=application/octet-stream"
-			},
-			{
-				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-				loader: "url?limit=10000&mimetype=image/svg+xml"
-			},
-			{
-				test: /\.gif/,
-				loader: "url-loader?limit=10000&mimetype=image/gif"
-			},
-			{
-				test: /\.jpg/,
-				loader: "url-loader?limit=10000&mimetype=image/jpg"
-			},
-			{
-				test: /\.png/,
-				loader: "url-loader?limit=10000&mimetype=image/png"
-			}
-		]
-	},
-	devServer: {
-		contentBase: "./public",
-			noInfo: true, //  --no-info option
-			hot: true,
-			inline: true
-		},
-	plugins: [
-		new webpack.NoErrorsPlugin()
-	]
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "file"
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                loader: "url?prefix=font/&limit=5000"
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/octet-stream"
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=image/svg+xml"
+            },
+            {
+                test: /\.gif/,
+                loader: "url-loader?limit=10000&mimetype=image/gif"
+            },
+            {
+                test: /\.jpg/,
+                loader: "url-loader?limit=10000&mimetype=image/jpg"
+            },
+            {
+                test: /\.png/,
+                loader: "url-loader?limit=10000&mimetype=image/png"
+            }
+        ]
+    },
+    devServer: {
+        contentBase: "./public",
+        noInfo: true, //  --no-info option
+        hot: true,
+        inline: true,
+        port:8081
+    },
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new OpenBrowserWebpackPlugin({ url: 'http://localhost:8081/' })
+
+    ]
 };
